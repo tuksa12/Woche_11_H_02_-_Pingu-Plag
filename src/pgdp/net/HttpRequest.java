d49dd7484e9551bc949feca42e5518f833937eee
DESKTOP-2KIND8H\Raphael Tuksa <rapha.tuksa@hotmail.com>
2021-02-05 18:42:26 +0100

    Test 2

--- src/pgdp/net/HttpRequest.java
+++ src/pgdp/net/HttpRequest.java
@@ -20,12 +20,39 @@ public final class HttpRequest {
     }
 
     public String getPath(){
-        return firstLine;
+        return firstLine.substring(firstLine.indexOf(System.lineSeparator()),firstLine.indexOf('?'));
     }
 
     public Map<String, String> getParameters(){
         Map<String, String> result = new HashMap<>();
-        result.put(firstLine, URLDecoder.decode(body, StandardCharsets.UTF_8));
+
+        boolean containsParameters = firstLine.contains("=");
+        String parametersFirstLine = firstLine.substring(firstLine.indexOf("?") +1, firstLine.lastIndexOf(" "));
+        while(containsParameters){
+            if (parametersFirstLine.contains("&")){
+                String parameter = parametersFirstLine.substring(0,parametersFirstLine.indexOf('&'));
+                result.put(URLDecoder.decode(parameter.substring(0,parameter.indexOf("=")),StandardCharsets.UTF_8),URLDecoder.decode(parameter.substring(parameter.indexOf("=") +1),StandardCharsets.UTF_8));
+                parametersFirstLine = parametersFirstLine.substring(parametersFirstLine.indexOf('&') + 1);
+            } else {
+                String parameter = parametersFirstLine;
+                result.put(URLDecoder.decode(parameter.substring(0,parameter.indexOf("=")),StandardCharsets.UTF_8),URLDecoder.decode(parameter.substring(parameter.indexOf("=") +1),StandardCharsets.UTF_8));
+                containsParameters = false;
+            }
+        }
+
+        containsParameters = body.contains("=");
+        String parametersBody = body.substring(body.indexOf('"') + 1, body.lastIndexOf('"'));
+        while(containsParameters){
+            if (parametersBody.contains("&")){
+                String parameter = parametersBody.substring(0,parametersBody.indexOf('&'));
+                result.put(URLDecoder.decode(parameter.substring(0,parameter.indexOf("=")),StandardCharsets.UTF_8),URLDecoder.decode(parameter.substring(parameter.indexOf("=") +1),StandardCharsets.UTF_8));
+                parametersBody = parametersBody.substring(parametersBody.indexOf('&') + 1);
+            } else {
+                String parameter = parametersBody;
+                result.put(URLDecoder.decode(parameter.substring(0,parameter.indexOf("=")),StandardCharsets.UTF_8),URLDecoder.decode(parameter.substring(parameter.indexOf("=") +1),StandardCharsets.UTF_8));
+                containsParameters = false;
+            }
+        }
         return result;
     }
 }
